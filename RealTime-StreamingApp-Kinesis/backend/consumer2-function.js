06c236192f51f151f636778f88594ab45a0ffdb0
Pranav Swaroop M N <115005957+Pranav10ss@users.noreply.github.com>
2024-11-21 20:23:55 +0900

    Delete RealTime-StreamingApp-Kinesis directory

--- RealTime-StreamingApp-Kinesis/backend/consumer2-function.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-    console.log(JSON.stringify(event));
-    for (const record of event.Records) {
-        const data = JSON.parse(Buffer.from(record.kinesis.data, 'base64'));
-        //send emails to clients, publish stuff to social media
-        console.log('consumer #2', data);
-    }
-};
